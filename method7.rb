444ea6464b4df28ae082ae319eb341c7ff16b36c
Valerie Osei <valerie.osei@gmail.com>
2018-07-18 14:45:07 -0400

    defined method that wraps chosen texts in symbols.

---
+++ method7.rb
@@ -0,0 +1,13 @@
+def wrap_text(my_word, my_symbol)
+  p "#{my_symbol}#{my_word}#{my_symbol.reverse}" # -> adding .reverse makes the symbols symmetrical on either side
+end
+
+puts "What word would you like to wrap?"
+
+my_word = gets.chomp
+
+puts "What symbol(s) would you like to use to wrap your word?"
+
+my_symbol = gets.chomp
+
+wrap_text(my_word, my_symbol)
